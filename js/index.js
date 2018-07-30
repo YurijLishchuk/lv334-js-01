d41a62d6e4728c52a269ba5773974e1e48df93da
Yurij Lishchuk <yurijlishchuk@gmail.com>
2018-07-30 16:36:46 +0000

    initial

---
+++ js/index.js
@@ -0,0 +1,26 @@
+let hello = "Hello World";
+alert(hello);
+
+const userName = prompt('Enter your name');
+const greetings = 'Hello, ' + userName;
+alert(greetings);
+
+const userAge = parseInt(prompt('Enter your age'));
+alert('Your age is ' + userAge);
+
+const nextAge = userAge + 1;
+alert('Next year you will be: ' + nextAge);
+
+if (userAge != 18) {
+    alert('You are adult');
+} else {
+    alert('You are a child');
+}
+
+function add(a, b, c) {
+    const result = (a + b) + c;
+    return result;
+}
+
+const result = add(1, 2, 3);
+alert(result);
